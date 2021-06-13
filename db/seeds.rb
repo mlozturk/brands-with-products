fb6f62c3a6a9aa31af2a677aea3a7e8c22f1d0ad
mlozturk <emelozturk@live.com>
2021-06-13 19:04:29 +0300

    Update db to add brands and products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+Brand.destroy_all
+
+bosch = Brand.create(brand_name: 'Bosch', logo_url:'https://www.bosch-home.com/store/medias/sys_master/root/h72/h58/9828767989790/English-165px.jpg')
+siemens = Brand.create(brand_name: 'Siemens', logo_url:'https://www.siemens-home.bsh-group.com/store/medias/sys_master/root/h67/h7b/9646381301790/siemens-logo-180px.jpg')
+beko = Brand.create(brand_name: 'Beko', logo_url:'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/New_Beko_logo.svg/220px-New_Beko_logo.svg.png')
+ariston = Brand.create(brand_name: 'Ariston', logo_url:'https://seeklogo.com/images/A/Ariston-logo-EA3E5D9B4E-seeklogo.com.png')
+
+Product.create(name: 'Serie 6 free-standing fridge', brand: bosch)
+Product.create(name: 'IQ700 free-standing dishwasher', brand: siemens)
+Product.create(name: 'BK1798 dishwasher', brand: beko)
+Product.create(name: 'Ari87 ', brand: ariston)
+Product.create(name: 'Serie 4 10kg washing machine',  brand: bosch)
+Product.create(name: 'BK483 fridge', brand: beko)
+Product.create(name: 'Serie 4 freezer', brand: bosch)
