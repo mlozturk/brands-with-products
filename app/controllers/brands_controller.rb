c8ec355c51b3ea16b09a97914b229bca2e87cace
mlozturk <emelozturk@live.com>
2021-06-13 19:40:21 +0300

    Modify related brand files to see list of brands

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,13 +1,48 @@
 class BrandsController < ApplicationController
+  before_action :current_brand, only: [:show, :edit, :update, :destroy]
+
   def index
+    @brands = Brand.all
   end
 
   def show
+    
   end
 
   def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.create(brand_params)
+
+    redirect_to brand_path(@brand)
   end
 
   def edit
+    
   end
+
+  def update
+
+    @brand.update(brand_params)
+
+    redirect_to brand_path(@brand)
+  end
+
+  def destroy
+    @brand.destroy
+
+    redirect_to brands_path
+  end
+
+  private
+  def brand_params
+    params.require(:brand).permit(:brand_name, :logo_url)
+  end
+
+  def current_brand
+    @brand = Brand.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/edit'
-  get 'brands/index'
-  get 'brands/show'
-  get 'brands/new'
-  get 'brands/edit'
+  resources :brands, :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
